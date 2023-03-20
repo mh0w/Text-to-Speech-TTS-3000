47b158afb6b95ff5cbffec0b7d06a56210cadfcd
Hawkes <matthew.hawkes@ons.gov.uk>
2023-03-20 16:21:45 +0000

    Add launch_tts.bat required packages list

--- launch_tts.bat
+++ launch_tts.bat
@@ -1,13 +1,14 @@
+echo off
 cd /d C:\Users\Matthew\Documents\TTS 3000
+cd /d C:\Users\hawkem\OneDrive - Office for National Statistics\Tools\Text-to-Speech-TTS-3000
 
-echo CTRL+b to read highlighted text, play, or pause
-echo CTRL+i to stop
+cls
 
 set root=%USERPROFILE%/Anaconda3
 
 call %root%/Scripts/activate.bat %root%
 
-python -m pip install --no-input --user pyttsx3 pyautogui keyboard pygame
+python -m pip install --quiet --no-input --user pyttsx3 pyautogui keyboard pygame soundfile
 
 python main.py
 
