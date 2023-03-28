ef2ac8f05538bcebe16216018ea468b3d3c58169
Hawkes <matthew.hawkes@ons.gov.uk>
2023-03-28 15:02:11 +0100

    Fix bugs by adding missing dependencies

--- launch_tts.bat
+++ launch_tts.bat
@@ -3,12 +3,15 @@ cd /d C:\Users\Matthew\Documents\TTS 3000
 cd /d C:\Users\hawkem\OneDrive - Office for National Statistics\Tools\Text-to-Speech-TTS-3000
 
 cls
+echo Loading, please wait...
 
 set root=%USERPROFILE%/Anaconda3
 
 call %root%/Scripts/activate.bat %root%
 
-python -m pip install --quiet --no-input --user pyttsx3 pyautogui keyboard pygame soundfile
+python -m pip install --no-input --user -r requirements.txt
+
+cls
 
 python main.py
 
